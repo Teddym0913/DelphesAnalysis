67d15da133b8e4a85231640abd75760ceffc48fe
Wu Yongcheng <niepanchongsheng@126.com>
2016-10-21 11:33:29 +0800

    Modify the ignore file, such that LinkDef.h file will be included.

---
+++ include/TreeReaderLinkDef.h
@@ -0,0 +1,10 @@
+#include "TreeReader.h"
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TreeReader+;
+
+#endif
